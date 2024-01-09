975c0f2ea3c22e74d5e40234032c58f6651034c6
AlbertoCavans <alberto96cavani@gmail.com>
2024-01-09 02:52:52 +0100

    Feat - js progress

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 /* Const ref to main-title */
-const password = document.getElementById("main-title");
+const resultElement = document.getElementById("main-title");
 
 /* Name const */
 const utname = prompt("Qual è il tuo nome?");
@@ -12,3 +12,10 @@ console.log(utsurname);
 /* Pref-color const */
 const utprefcolor = prompt("Qual è il tuo colore preferito?");
 console.log(utprefcolor);
+
+/* Password construction */
+const Password = utname + utsurname + utprefcolor + "23";
+console.log(Password);
+
+/* Control password */
+resultElement.innerHTML = "La tua password sarà:" + " " + Password;
