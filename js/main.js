c415e84cd3a20eeb9f61f5a1adc4a69d9e7ab5bd
AlbertoCavans <alberto96cavani@gmail.com>
2024-01-09 02:39:22 +0100

    Feat - js

---
+++ js/main.js
@@ -0,0 +1,14 @@
+/* Const ref to main-title */
+const password = document.getElementById("main-title");
+
+/* Name const */
+const utname = prompt("Qual è il tuo nome?");
+console.log(utname);
+
+/* Surname const */
+const utsurname = prompt("Qual è il tuo cognome?");
+console.log(utsurname);
+
+/* Pref-color const */
+const utprefcolor = prompt("Qual è il tuo colore preferito?");
+console.log(utprefcolor);
